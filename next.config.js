2d84acea58a768d91c6420e316fb39b2f3878c5c
otacleT <mytspe46@gmail.com>
2024-06-18 08:33:43 +0900

    outputStandalone

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
+  experimental: {
+    outputStandalone: true, // ポイントはここ
+  },
 };
 
 module.exports = nextConfig;
